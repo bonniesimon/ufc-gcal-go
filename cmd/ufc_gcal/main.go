15fa43bbabf7a1cf00670e5bb4868c028ca6dbc7
Bonnie Simon <bonnie.developer@gmail.com>
2024-11-24 19:47:03 +0530

    Add prettyPrint events

--- cmd/ufc_gcal/main.go
+++ cmd/ufc_gcal/main.go
@@ -18,8 +18,8 @@ type Event struct {
 type Fight struct {
 	Main     bool    `json:"main"`
 	Weight   string  `json:"weight"`
-	FighterA Fighter `json:"fighter_a"`
-	FighterB Fighter `json:"fighter_b"`
+	FighterA Fighter `json:"fighterA"`
+	FighterB Fighter `json:"fighterB"`
 }
 
 type Fighter struct {
@@ -30,7 +30,7 @@ type Fighter struct {
 type MMAFightCardAPIResponse struct {
 	ID        string  `json:"id"`
 	Data      []Event `json:"data"`
-	UpdatedAt string  `json:"updated_at"`
+	UpdatedAt string  `json:"updatedAt"`
 }
 
 func getEvents() []Event {
@@ -59,8 +59,27 @@ func getEvents() []Event {
 	return apiRes.Data
 }
 
+func prettyPrintEvents(event Event) {
+	fmt.Printf("\n\n\n\n")
+	fmt.Printf(`
+		------------------------------- %s -------------------------------
+		%s
+		%s
+	`, event.Title, event.Date, event.Link)
+
+	for i := 0; i < len(event.Fights); i++ {
+		fmt.Printf(`
+			%s vs %s
+		`, event.Fights[i].FighterA.Name, event.Fights[i].FighterB.Name)
+	}
+
+	fmt.Println("------------------------------------------------------------------")
+}
+
 func main() {
 	events := getEvents()
 
-	fmt.Println(events[0])
+	for i := 0; i < len(events); i++ {
+		prettyPrintEvents(events[i])
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bonniesimon/ufc-gcal-go
 
 go 1.22.4
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
